4fa4b222be4097a04b01ea7001321517530e646b
punisher-303 <mehafin0@gmail.com>
2025-11-11 22:48:45 +0530

    fix: Update version for 4khdHub provider

--- providers/4khdhub/catalog.ts
+++ providers/4khdhub/catalog.ts
@@ -1,19 +1,19 @@
 export const catalog = [
   {
-    title: "Popular Movies",
+    title: "Home",
     filter: "/category/new-movies-10810.html",
   },
   {
     title: "Latest TV Shows",
-    filter: "/category/new-series-10811.html",
+    filter: "/category/new-series-10811",
   },
   {
     title: "Anime",
-    filter: "/category/anime-10812.html",
+    filter: "/category/anime-10812",
   },
   {
     title: "4K HDR",
-    filter: "/category/4k-hdr-10776.html",
+    filter: "/category/4k-hdr-10776",
   },
 ];
 
