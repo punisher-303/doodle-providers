f4ad0511d9c3b11a5d05bfa38fb18d3429f647e4
punisher-303 <mehafin0@gmail.com>
2025-11-14 23:16:34 +0530

    fix

--- providers/4khdhub/catalog.ts
+++ providers/4khdhub/catalog.ts
@@ -1,19 +1,19 @@
 export const catalog = [
   {
-    title: "Home",
+    title: "Popular Movies",
     filter: "/category/new-movies-10810.html",
   },
   {
     title: "Latest TV Shows",
-    filter: "/category/new-series-10811",
+    filter: "/category/new-series-10811.html",
   },
   {
     title: "Anime",
-    filter: "/category/anime-10812",
+    filter: "/category/anime-10812.html",
   },
   {
     title: "4K HDR",
-    filter: "/category/4k-hdr-10776",
+    filter: "/category/4k-hdr-10776.html",
   },
 ];
 
