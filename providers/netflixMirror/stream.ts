d581547ca003c1d3ac8e221668cac9d732b186ff
punisher-303 <mehafin0@gmail.com>
2025-12-03 12:51:57 +0530

    fix net mirror

--- providers/netflixMirror/stream.ts
+++ providers/netflixMirror/stream.ts
@@ -12,7 +12,7 @@ export const getStream = async ({
     let providerValue = "netflixMirror";
     const baseUrl = await getBaseUrl("nfMirror");
     console.log("nfGetStream, baseUrl:", baseUrl);
-    const url = `https://netmirror.8man.dev/api/net-proxy?url=${baseUrl}${
+    const url = `https://net-cookie-kacj.vercel.app/api/netmirror?url=${baseUrl}${
       providerValue === "netflixMirror"
         ? "/mobile/playlist.php?id="
         : "/pv/playlist.php?id="
