528aa36dca7c3993bb9e597c9eaf6b48b93f8a5a
punisher-303 <mehafin0@gmail.com>
2025-12-31 21:09:13 +0530

    fix

---
+++ providers/cinestream/catalog.ts
@@ -0,0 +1,14 @@
+export const catalog = [
+  {
+    title: "Trending Movies",
+    filter: "movie",
+  },
+  {
+    title: "Trending Series",
+    filter: "series",
+  },
+  {
+    title: "Top Anime",
+    filter: "anime",
+  },
+];
