b08c6bcf355365504a5f6961ece7572a99a49966
punisher-303 <mehafin0@gmail.com>
2026-01-31 22:37:44 +0530

    update

--- dist/HDMovie2/catalog.js
+++ dist/HDMovie2/catalog.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.catalog=void 0,exports.catalog=[{title:"Trending",filter:""},{title:"Anime",filter:"genre/animation/"}];
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.catalog=void 0,exports.catalog=[{title:"Trending",filter:""},{title:"Anime",filter:"movie-genre/anime/"}];
--- dist/ridoMovies/catalog.js
+++ dist/ridoMovies/catalog.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.genres=exports.catalog=void 0,exports.catalog=[{title:"Popular Movies",filter:""},{title:"Featured Movies",filter:"genre/animation/"}],exports.genres=[];
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.genres=exports.catalog=void 0,exports.catalog=[{title:"Popular Movies",filter:"genre/HOLLYWOOD-MOVIES/"},{title:"Featured Movies",filter:"genre/animation/"}],exports.genres=[];
